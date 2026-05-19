4988f7e04c39535bbc62febc584198f4f9fdb5b2
faith-fwzzz <muhammad.fawwaz003@binus.ac.id>
2026-05-19 14:33:53 +0700

    Initial Commit

---
+++ MoneyPortfolioApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyPortfolioApp
+//
+//  Created by FWZ on 19/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
